5186aee7b7d9083e99a5f7f2037fb785f6f3fd6d
yasudaProduct <yasu.private.ct@gmail.com>
2024-03-19 01:34:04 +0900

    HttpClient

---
+++ Merino/Infrastructure/MerinoApiClient.cs
@@ -0,0 +1,14 @@
+﻿namespace Merino.Infrastructure
+{
+    public class MerinoApiClient
+    {
+        private static readonly HttpClient _client;
+
+        static MerinoApiClient()
+        {
+            _client = new HttpClient();
+        }
+
+        public HttpClient client { get { return _client; } }
+    }
+}
