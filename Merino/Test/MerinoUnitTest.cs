bd0c6c6981e4515c2b915c742d16f26d71902d35
yasudaProduct <yasu.private.ct@gmail.com>
2024-03-27 13:38:32 +0900

    UnitTestの基底クラスを追加

---
+++ Merino/Test/MerinoUnitTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Merino.Test
+{
+    public class MerinoUnitTest : IDisposable
+    {
+        public void Dispose()
+        {
+            // 完了後にアンマネージドリソースの処理したり
+            Console.WriteLine("disposed");
+        }
+    }
+}
